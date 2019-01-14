6b6588b6e0fd6f5f77ee1ed1caca640cf6f65683
Batman.qin <943104990@qq.com>
2019-01-14 14:28:37 +0800

    consul update

---
+++ service-config/src/main/java/com/fast/consule/config/ServiceConfigApplication.java
@@ -0,0 +1,30 @@
+package com.fast.consule.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class ServiceConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceConfigApplication.class, args);
+    }
+
+    // 读取远程配置
+    @Value("${bar.foo.name}")
+    private String name;
+
+    // 将配置展示在页面
+    @GetMapping("/getName")
+    public String getName(){
+        return name;
+    }
+
+}
+
