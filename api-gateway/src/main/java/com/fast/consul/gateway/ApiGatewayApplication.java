5b287f9617af750e90b23cf0a6e78bcb8d91cf69
Batman.qin <943104990@qq.com>
2019-01-22 16:38:11 +0800

    consul config update

--- api-gateway/src/main/java/com/fast/consul/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/fast/consul/gateway/ApiGatewayApplication.java
@@ -1,13 +1,29 @@
 package com.fast.consul.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RefreshScope
+@RestController
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    // 读取远程配置
+    @Value("${bar.foo.name}")
+    private String name;
+
+    // 将配置展示在页面
+    @GetMapping("/getName")
+    public String getName(){
+        return name;
     }
 
 }
